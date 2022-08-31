33a243493082674120206b071bb50a20b1372e2e
xiaobang <xiaobang@521.com>
2022-08-31 21:21:32 +0800

    第三次提交

--- src/main/java/com/xiaobang/GitTest.java
+++ src/main/java/com/xiaobang/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好呀");
         System.out.println("好呀2");
+        System.out.println("push test");
     }
 }
