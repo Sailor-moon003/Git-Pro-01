94731159c6fda67bf433dfb799f560dd14d66b4b
xiaobang <xiaobang@521.com>
2022-08-31 20:13:37 +0800

    第一次提交

---
+++ src/main/java/com/xiaobang/GitTest.java
@@ -0,0 +1,12 @@
+package com.xiaobang;
+
+/**
+ * @author xiaobang
+ * @create 2022-08-31 20:11
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好呀");
+    }
+}
